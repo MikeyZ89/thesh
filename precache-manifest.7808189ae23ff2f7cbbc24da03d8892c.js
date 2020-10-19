589b6351543efd1043484c67d796c8fe940007a1
Michael Zlotkowski <michael.zlotkowski@gmail.com>
2020-10-19 11:56:13 -0700

    Updates

---
+++ precache-manifest.7808189ae23ff2f7cbbc24da03d8892c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4e8b11e3ac369010fce7913f98f19db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9a5d422c13f45396032",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "246bf40496224eb2576a",
+    "url": "/static/js/2.15e519e6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.15e519e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9a5d422c13f45396032",
+    "url": "/static/js/main.30bc1409.chunk.js"
+  },
+  {
+    "revision": "653965a3967cfc416b16",
+    "url": "/static/js/runtime-main.40574994.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
